25c35c4f8a47dd5190f38e5c4b2373671d502ad3
SerArias <99254373+SerArias@users.noreply.github.com>
2022-03-19 11:01:38 +0100

    Método get
    Método get de ver detalles básicos de publicación, y creación de método vacío en el DAOPublicacion leerPublicacion() para poder llamarlo desde el método get

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 "use strict";
 
+//Librerías locales
+const DAOPublicacion = require("./public/javascripts/DAOPublicacion");
+const config = require("./config");
+
+//Librerías externas
+const express = require("express");
+const app = express();
+
+//Constantes para la base de datos
+const mysql = require("mysql");
+const { append } = require("express/lib/response");
+const express = require("express");
+const pool = mysql.createPool({
+	host: config.host,
+	user: config.user,
+	password: config.password,
+	database: config.database
+});
+
+
+//Vista de datos básicos de publicación
+app.get("/leerPublicacion", function(request, response){
+	let daoPublicacion = new DAOPublicacion(pool);
+	daoPublicacion.leerPublicacion(function(err, result) {
+		if(err) {
+			console.log(err);
+			let busq = "Ha ocurrido un error: " + err;
+			response.render("indice", {usuario: request.session.currentUser, nick: request.session.userNick, img: request.session.img, 
+				busqueda: busq, array: []});
+		}
+		else {
+			let busq = "Vista de publicación";
+			response.render("indice", {usuario: request.session.currentUser, nick: request.session.userNick, img: request.session.img, 
+				busqueda: b, array: result});
+		}
+	});
+});
+
   function operar(i1, op, i2){
 	let res;
 
