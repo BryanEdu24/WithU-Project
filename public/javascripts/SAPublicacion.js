7e9a67f42eaf68d300dd540517dadb4706b6b10e
Lurngi <adrpriet@ucm.es>
2022-03-21 17:39:48 +0100

    POST y SA agregarPublicacion

--- public/javascripts/SAPublicacion.js
+++ public/javascripts/SAPublicacion.js
@@ -7,7 +7,23 @@ class SAPublicacion {
 	}
     
 	agregarPublicacion(publicacion, callback) { //Publicación debería ser una estructura {titulo, cuerpo}
-		console.log("TO-DO");
+		if(publicacion.titulo !== "" || publicacion.cuerpo !== ""){
+			callback("No puede haber campos vacios");
+		}
+		else{
+			if(publicacion.titulo.length > 50){
+				callback("El titulo tiene más de 50 caracteres");
+			}
+			else{
+				if(publicacion.cuerpo.length < 90){
+					callback("El cuerpo es demasiado corto");
+				}
+				else{
+					let dao = new DAOPublicacion(pool);
+					dao.agregarPublicacion(publicacion, callback);
+				}
+			}
+		}
 	}
 
 	leerPublicacion(id, callback) {
