0cec4bbc321053f1add5393c7004ef0d5893466a
josatoo <joseansu@ucm.es>
2022-03-22 18:22:16 +0100

    SAPublicacion tets de integracion de seccion

--- tests/SAPublicacion.test.js
+++ tests/SAPublicacion.test.js
@@ -14,6 +14,8 @@ const sa = new SAPublicacion(pool);
 const tituloCorrecto = "Prueba titulo correcto con menos de 50 caracteres";
 const tituloIncorrecto = "Prueba titulo incorrecto con más de 50 caracteres. Debería fallar";
 const cuerpoCorrecto = "Este cuerpo tiene más de 90 caracteres: \n\nLorem ipsum dolor sit amet consectetur adipiscing elit aliquet est, facilisi nascetur nulla blandit malesuada varius fermentum hac, ultricies pulvinar cubilia platea massa fames enim iaculis. Ridiculus himenaeos lacinia nullam platea placerat netus sagittis habitasse sollicitudin ut viverra, tristique duis laoreet quam ad malesuada in iaculis magna nisl, leo quis facilisi congue mus odio proin feugiat dictum natoque. Ad dignissim nullam sed himenaeos vulputate inceptos rutrum molestie, pretium potenti tempor nibh porta cum hendrerit, at dictumst hac convallis tortor netus viverra.\n\nTempor enim dictum facilisi netus aliquam torquent justo bibendum pellentesque curae vestibulum massa eros ac ante, vehicula class erat leo inceptos luctus nibh maecenas diam libero dapibus felis ornare. Aenean maecenas metus ullamcorper lacinia nascetur aliquam justo vitae, suscipit arcu malesuada volutpat nulla class tristique facilisi, ultrices ante turpis commodo lectus hac rhoncus. Tortor neque velit montes interdum ridiculus eget arcu magna, urna dignissim cursus eleifend class varius venenatis, leo accumsan nulla maecenas at litora quis.";
+const seccionCorrecta = 1;
+const seccionIncorrecta = 0;
 
 test('Intentamos agregar un objeto undefined', done =>{
     function cb(err, ID){
@@ -68,11 +70,45 @@ test('Intentamos agregar un objeto que no es una publicación', done =>{
     }
 });
 
-test('Intentamos agregar una publicación con titulo y cuerpo correctos', done =>{
+test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccion correctas', done =>{
     function cb(err, ID){
         try{
             expect(err).toBe(null);
             expect(ID).toBeGreaterThan(0);
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
+
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto, seccion:seccionCorrecta};
+    try{
+        sa.agregarPublicacion(publicacion, cb);
+    }catch(error){
+        done(error);
+    }
+});
+test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccion incorrectas', done =>{
+    function cb(err, ID){
+        try{
+            expect(err).toBe("La seccion no es correcta");
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
+
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto,seccion:seccionIncorrecta};
+    try{
+        sa.agregarPublicacion(publicacion, cb);
+    }catch(error){
+        done(error);
+    }
+});
+test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccion undefined', done =>{
+    function cb(err, ID){
+        try{
+            expect(err).toBe("No puede haber campos vacios");
             done();
         }catch(error){
             done(error);
@@ -86,7 +122,23 @@ test('Intentamos agregar una publicación con titulo y cuerpo correctos', done =
         done(error);
     }
 });
+test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccion null', done =>{
+    function cb(err, ID){
+        try{
+            expect(err).toBe("No puede haber campos vacios");
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
 
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto,seccion:null};
+    try{
+        sa.agregarPublicacion(publicacion, cb);
+    }catch(error){
+        done(error);
+    }
+});
 test('Intentamos agregar una publicación con titulo vacío', done =>{
     function cb(err, ID){
         try{
