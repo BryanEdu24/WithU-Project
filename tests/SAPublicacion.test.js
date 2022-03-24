981e033a05244c2dd176fb83791e7118c21431c1
Fran <fcubeddev@gmail.com>
2022-03-25 00:27:11 +0100

    Agregada etiqueta a los test de integración

--- tests/SAPublicacion.test.js
+++ tests/SAPublicacion.test.js
@@ -16,6 +16,8 @@ const tituloIncorrecto = "Prueba titulo incorrecto con más de 50 caracteres. De
 const cuerpoCorrecto = "Este cuerpo tiene más de 90 caracteres: \n\nLorem ipsum dolor sit amet consectetur adipiscing elit aliquet est, facilisi nascetur nulla blandit malesuada varius fermentum hac, ultricies pulvinar cubilia platea massa fames enim iaculis. Ridiculus himenaeos lacinia nullam platea placerat netus sagittis habitasse sollicitudin ut viverra, tristique duis laoreet quam ad malesuada in iaculis magna nisl, leo quis facilisi congue mus odio proin feugiat dictum natoque. Ad dignissim nullam sed himenaeos vulputate inceptos rutrum molestie, pretium potenti tempor nibh porta cum hendrerit, at dictumst hac convallis tortor netus viverra.\n\nTempor enim dictum facilisi netus aliquam torquent justo bibendum pellentesque curae vestibulum massa eros ac ante, vehicula class erat leo inceptos luctus nibh maecenas diam libero dapibus felis ornare. Aenean maecenas metus ullamcorper lacinia nascetur aliquam justo vitae, suscipit arcu malesuada volutpat nulla class tristique facilisi, ultrices ante turpis commodo lectus hac rhoncus. Tortor neque velit montes interdum ridiculus eget arcu magna, urna dignissim cursus eleifend class varius venenatis, leo accumsan nulla maecenas at litora quis.";
 const seccionCorrecta = 1;
 const seccionIncorrecta = 0;
+const etiquetaIncorrecta = [];
+const etiquetaCorrecta = ["Emociones"];
 
 test('Intentamos agregar un objeto undefined', done =>{
     function cb(err, ID){
@@ -70,7 +72,7 @@ test('Intentamos agregar un objeto que no es una publicación', done =>{
     }
 });
 
-test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccion correctas', done =>{
+test('Intentamos agregar una publicación con titulo, cuerpo, seccion y etiquetas correctas', done =>{
     function cb(err, ID){
         try{
             expect(err).toBe(null);
@@ -81,14 +83,14 @@ test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccio
         }
     }
 
-    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto, seccion:seccionCorrecta};
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto, seccion:seccionCorrecta, etiqueta: etiquetaCorrecta};
     try{
         sa.agregarPublicacion(publicacion, cb);
     }catch(error){
         done(error);
     }
 });
-test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccion incorrectas', done =>{
+test('Intentamos agregar una publicación con titulo ,etiqueta y cuerpo correctos y seccion incorrectas', done =>{
     function cb(err, ID){
         try{
             expect(err).toBe("La seccion no es correcta");
@@ -98,14 +100,14 @@ test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccio
         }
     }
 
-    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto,seccion:seccionIncorrecta};
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto, seccion:seccionIncorrecta, etiqueta: etiquetaCorrecta};
     try{
         sa.agregarPublicacion(publicacion, cb);
     }catch(error){
         done(error);
     }
 });
-test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccion undefined', done =>{
+test('Intentamos agregar una publicación con titulo ,etiqueta y cuerpo correctos y seccion undefined', done =>{
     function cb(err, ID){
         try{
             expect(err).toBe("No puede haber campos vacios");
@@ -115,14 +117,14 @@ test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccio
         }
     }
 
-    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto};
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto, seccion: undefined, etiqueta: etiquetaCorrecta};
     try{
         sa.agregarPublicacion(publicacion, cb);
     }catch(error){
         done(error);
     }
 });
-test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccion null', done =>{
+test('Intentamos agregar una publicación con titulo, etiqueta y cuerpo correctos y seccion null', done =>{
     function cb(err, ID){
         try{
             expect(err).toBe("No puede haber campos vacios");
@@ -132,13 +134,68 @@ test('Intentamos agregar una publicación con titulo , cuerpo correctos y seccio
         }
     }
 
-    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto,seccion:null};
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto,seccion:null, etiqueta: etiquetaCorrecta};
     try{
         sa.agregarPublicacion(publicacion, cb);
     }catch(error){
         done(error);
     }
 });
+
+test('Intentamos agregar una publicación con titulo, cuerpo, seccion correcta y etiqueta incorrecta', done =>{
+    function cb(err, ID){
+        try{
+            expect(err).toBe("Se debe añadir al menos una etiqueta");
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
+
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto, seccion: seccionCorrecta, etiqueta: etiquetaIncorrecta};
+    try{
+        sa.agregarPublicacion(publicacion, cb);
+    }catch(error){
+        done(error);
+    }
+});
+
+test('Intentamos agregar una publicación con titulo, cuerpo, seccion correcta y etiqueta null', done =>{
+    function cb(err, ID){
+        try{
+            expect(err).toBe("No puede haber campos vacíos");
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
+
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto, seccion: seccionCorrecta, etiqueta: null};
+    try{
+        sa.agregarPublicacion(publicacion, cb);
+    }catch(error){
+        done(error);
+    }
+});
+
+test('Intentamos agregar una publicación con titulo, cuerpo, seccion correcta y etiqueta undefined', done =>{
+    function cb(err, ID){
+        try{
+            expect(err).toBe("No puede haber campos vacíos");
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
+
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto, seccion: seccionCorrecta, etiqueta: undefined};
+    try{
+        sa.agregarPublicacion(publicacion, cb);
+    }catch(error){
+        done(error);
+    }
+});
+
 test('Intentamos agregar una publicación con titulo vacío', done =>{
     function cb(err, ID){
         try{
@@ -149,7 +206,7 @@ test('Intentamos agregar una publicación con titulo vacío', done =>{
         }    
     }
     
-    let publicacion = { titulo: "", cuerpo: cuerpoCorrecto};
+    let publicacion = { titulo: "", cuerpo: cuerpoCorrecto, seccion: seccionCorrecta, etiqueta: etiquetaCorrecta};
     try{
         sa.agregarPublicacion(publicacion, cb);
     }catch(error){
@@ -167,7 +224,7 @@ test('Intentamos agregar una publicación con cuerpo vacio', done =>{
         }    
     }
 
-    let publicacion = { titulo: tituloCorrecto, cuerpo: "" };
+    let publicacion = { titulo: tituloCorrecto, cuerpo: "" , seccion: seccionCorrecta, etiqueta: etiquetaCorrecta};
     try{
         sa.agregarPublicacion(publicacion, cb);
     }catch(error){
@@ -185,7 +242,7 @@ test('Intentamos agregar una publicación con titulo incorrecto', done =>{
         }    
     }
 
-    let publicacion = { titulo: tituloIncorrecto, cuerpo: cuerpoCorrecto };
+    let publicacion = { titulo: tituloIncorrecto, cuerpo: cuerpoCorrecto, seccion: seccionCorrecta, etiqueta: etiquetaCorrecta };
     try{
         sa.agregarPublicacion(publicacion, cb);
     }catch(error){
@@ -199,15 +256,17 @@ test('Leer datos de una publicación', done => {
     function callback(err, publicacion) { 
         try{
             expect(err).toBe(null);
-            expect(publicacion.Titulo).toBe(tituloCorrecto);
-            expect(publicacion.Cuerpo).toBe(cuerpoCorrecto);
+            expect(publicacion.titulo).toBe(tituloCorrecto);
+            expect(publicacion.cuerpo).toBe(cuerpoCorrecto);
+            expect(publicacion.seccion).toBe(seccionCorrecta);
+            expect(publicacion.etiqueta).toBe(etiquetaCorrecta);
             done(); 
         }catch(error){
             done(error);
         }  
     }
 
-    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto };
+    let publicacion = { titulo: tituloCorrecto, cuerpo: cuerpoCorrecto, seccion: seccionCorrecta, etiqueta: etiquetaCorrecta };
     
     try{
         sa.agregarPublicacion(publicacion, function (err, ID) {
