3bf3c40277c7ea6be6b528777bd3af0486a73f3a
SaMuelete01 <68590581+SaMuelete01@users.noreply.github.com>
2022-03-25 10:52:32 +0100

    script para jenkins

---
+++ configtest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm config.js
+
+echo "'use strict';
+module.exports = {
+        host: 'localhost',
+        user: 'withuroot',
+        password: 'withugps2022',
+        database: 'withu'
+}" > config.js
