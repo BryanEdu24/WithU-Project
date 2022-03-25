54c903310374a6881928316e34906109a45d07ba
CarloDubini <45142126+CarloDubini@users.noreply.github.com>
2022-03-25 10:52:13 +0100

    Create DAOSeccion.js

---
+++ public/javascripts/DAOSeccion.js
@@ -0,0 +1,106 @@
+"use strict";
+const DAOEtiqueta = require("./DAOEtiqueta");
+const DAOPublicacionEtiqueta = require("./DAOPublicacionEtiqueta");
+class DAOSeccion {
+	constructor(pool) {
+		this._pool = pool;
+	}
+	
+	agregarSeccion(seccion, callback) { //Seccion debería ser una estructura {nombre}
+		this._pool.getConnection(function(err, connection) {
+			if (err) {
+				connection.release();
+				callback("Error de conexion a la base de datos");
+			}
+			else {
+				connection.query("INSERT INTO seccion (nombre) VALUES (?)",  [ Seccion.nombre], //Aquí va la query a la BD
+					function(err, result) {
+						connection.release();
+						if (err) {
+							callback("Los datos no son correctos.");
+						}
+						else {
+							//Aquí se tratan los datos y llama al callback (Habría que devolver el ID generado por el instert)
+							callback(null, result.insertId);
+						}
+					}
+				);
+			}
+		});
+	}
+
+	leerPublicacion(ID,callback) {
+		this._pool.getConnection(function(err, connection) {
+			if (err) {
+				connection.release();
+				callback(new Error("Error de conexion a la base de datos"));
+			}
+			else {
+				connection.query("SELECT * FROM Seccion WHERE ID=?" ,[ID] ,//Aquí va la query a la BD
+					function(err, rows) {
+						connection.release();
+						if (err) {
+							callback(new Error("Error de conexion a la base de datos"));
+						}
+						else {
+							//Aquí se tratan los datos y llama al callback (Habría que devolver el ID generado por el insert)
+							let Seccion={ID:rows[0].ID,Nombre:rows[0].Nombre};
+							callback(null,Seccion);
+						}
+					}
+				);
+			}
+		});
+	}
+/*
+	listarSeccion(callback){
+		this._pool.getConnection(function(err, connection) {
+			if (err) {
+				connection.release();
+				callback(new Error("Error de conexion a la base de datos"));
+			}
+			else {
+				connection.query("SELECT * FROM Seccion", //Aquí va la query a la BD
+					function(err, rows) {
+						connection.release();
+						if (err) {
+							callback(new Error("Error de conexion a la base de datos"));
+						}
+						else {
+							let listaSecciones = Array.from(new Set(
+								rows.map(l => l.ID))).map(id => {
+									return {
+										ID: id, 
+										Nombre: rows.find(l => l.id === id).Nombre,
+									}
+								});
+							callback(null,listaSecciones);
+						}
+					}
+				);
+			}
+		});
+	}
+*/
+}
+module.exports = DAOSeccion;
+
+/*const mysql = require("mysql");
+const pool = mysql.createPool({
+	host: "localhost",
+	user: "root",
+	password: "",
+	database: "withu"
+});
+function leer(){
+	let dao= new DAOSeccion(pool);
+	dao.leerSeccion(1,function(err,pub){
+		if(err){
+			console.log(err);
+		}
+		else{
+			console.log(pub);
+		}
+	});
+}
+leer();*/
