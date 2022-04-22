5755a11d6ffbf97956757aace43934ebf45fcccf
CarloDubini <45142126+CarloDubini@users.noreply.github.com>
2022-04-22 23:50:28 +0200

    Create DAOSeccion.test.js

---
+++ tests/DAOSeccion.test.js
@@ -0,0 +1,118 @@
+const { TestWatcher } = require("jest");
+const DAOSeccion = require("../public/javascripts/DAOSeccion");
+const config = require("../config");
+const mysql = require("mysql");
+const pool = mysql.createPool({
+	host: config.host,
+	user: config.user,
+	password: config.password,
+	database: config.database
+});
+jest.setTimeout(5000);
+const dao = new DAOSeccion(pool);
+
+
+const Seccion1= "Estudios";
+const Seccion2= "Trabajo";
+const Seccion3= "Familia";
+const Seccion4= "Relaciones";
+const Seccion5= "Autoestima";
+const Seccion6= "Otros";
+const SeccionIncorrecta= "no problem";
+
+
+
+test('Leemos una seccion correctamente',done =>{
+    function cb(err,seccion){
+        try{
+            expect(err).toBe(null);
+            expect(seccion.Nombre).toBe(Seccion1);
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
+    
+    try{
+        dao.leerSeccion(1, cb);
+    } catch(err){
+        done(error);
+    }
+
+});
+
+test('Leemos una seccion Incorrectamente',done =>{
+    function cb(err,seccion){
+        try{
+            expect(err).toBe(null);
+            expect(seccion.Nombre).toBe(SeccionIncorrecta);
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
+    
+    try{
+        dao.leerSeccion(1, cb);
+    } catch(err){
+        done(error);
+    }
+
+});
+
+test('Leemos todas las secciones correctamente', done =>{
+    function cb(err,secciones){
+        try{
+            expect(err).toBe(null);
+            expect(secciones[0].Nombre).toBe(Seccion1);
+            expect(secciones[1].Nombre).toBe(Seccion2);
+            expect(secciones[2].Nombre).toBe(Seccion3);
+            expect(secciones[3].Nombre).toBe(Seccion4);
+            expect(secciones[4].Nombre).toBe(Seccion5);
+            expect(secciones[5].Nombre).toBe(Seccion6);
+            done();
+        }catch(error){
+            done(error);
+        }
+    }
+
+    try{
+        dao.leerSeccion(cb);
+    } catch(err){
+        done(error);
+    }
+});
+
+
+
+test('Error de conexión a la BBDD', done =>{
+    function cb(err, ID){
+        try{
+            expect(err).toBe("Error al obtener la conexión");
+            done();
+        }catch(error){
+            done(error);
+        }    
+    }
+    
+    let dao2 = new DAOSeccion();
+    try{
+        dao2.leerSeccion(1, cb);
+    } catch(err){
+        done();
+    }
+});
+
+
+test('Error al ejecutar la petición a la BBDD', done =>{
+    function cb(err, ID){
+        try{
+            expect(err).toBe("Los datos no son correctos.");
+            done();
+        }catch(error){
+            done(error);
+        }    
+    }
+    
+    dao.leerSeccion(9, cb);
+});
